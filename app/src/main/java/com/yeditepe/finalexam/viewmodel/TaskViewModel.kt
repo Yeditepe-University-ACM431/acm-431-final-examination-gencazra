947bd6c278675c0e71873a4f32e4298e6ba495b5
azragenc <azragenc04@gmail.com>
2026-01-06 14:47:02 +0300

    Add initial tasks and fetch from repository
    Initialize tasks with two sample Task objects and fetch tasks from the repository in the ViewModel's init block.

--- app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
@@ -1,13 +1,28 @@
 package com.yeditepe.finalexam.viewmodel
-
+//tamam
 import androidx.lifecycle.ViewModel
 
 class TaskViewModel : ViewModel() {
 
     // TODO 1: Create a mutable state list of Task objects
     // Initially add at least 2 tasks
+    var tasks = mutableStateListOf(
+        Task(id = 1, title = "Final Project by Azra", isCompleted = false),
+        Task(id = 2, title = "Check API by Azra", isCompleted = false)
+    )
+
+    init {
+        viewModelScope.launch {
+            apiTasks.value = repository.fetchTasks()
+        }
+    }
 
     fun toggleTask(taskId: Int) {
         // TODO 2: Update isCompleted for the given task
+        val index = tasks.indexOfFirst { it.id == taskId }
+        if (index != -1) {
+            val task = tasks[index]
+            tasks[index] = task.copy(isCompleted = !task.isCompleted)
+        }
     }
 }
