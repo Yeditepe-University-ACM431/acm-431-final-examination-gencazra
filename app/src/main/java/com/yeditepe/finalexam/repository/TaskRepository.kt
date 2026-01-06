ca2a465421e27c0ea50746e75d0332e1df50cb13
azragenc <azragenc04@gmail.com>
2026-01-06 14:48:58 +0300

    Implement fetchTasks to retrieve and map tasks

--- app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
+++ app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
@@ -2,12 +2,14 @@ package com.yeditepe.finalexam.repository
 
 import com.yeditepe.finalexam.api.TaskApi
 import com.yeditepe.finalexam.model.Task
-
+//burası da tamam
 class TaskRepository(private val api: TaskApi) {
 
     suspend fun fetchTasks(): List<Task> {
         // TODO 1: Call API
         // TODO 2: Convert TaskDto list to Task list
+        val dtoList = api.getTasks()
+        return dtoList.map { Task(it.id, it.title, it.completed) }
         return TODO("Provide the return value")
     }
 }
